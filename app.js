465754c1bf5eaaebfdcbc839ac69123495549abb
David Miotti <david@muxumuxu.com>
2017-11-30 11:42:37 +0100

    [+] Added POST route

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ require('dotenv').config()
 const keyPublishable = process.env.PUBLISHABLE_KEY
 const keySecret = process.env.SECRET_KEY
 const app = require('express')()
+const bodyParser = require('body-parser')
 const stripe = require('stripe')(keySecret)
 
-const params = {
+app.use(bodyParser.json()); // for parsing application/json
+
+const testParams = {
   number: '4242 4242 4242 4242',
   expMonth: 12,
   expYear: 21,
@@ -14,10 +17,34 @@ const params = {
 
 // Routes
 app.get('/', (req, res) =>
-  generateToken(params)
+  generateToken(testParams) // Using test params, we should use `req.params`
     .then(createCharge)
-    .then(token => res.status(201).send({ token }))
+    .then(charge => res.status(201).send({ charge }))
     .catch(error => res.status(422).send({ error }))
+)
+
+/*
+  POST '/'
+  {
+    "number": "4242 4242 4242 4242",
+    "expMonth": 12,
+    "expYear": 21,
+    "cvc": "123"
+  }
+
+  Example :
+
+  curl -i \
+    -X POST \
+    -H 'Content-Type: application/json' \
+    -d '{ "number": "4242 4242 4242 4242", "expMonth": 12, "expYear": 21, "cvc": "123" }' \
+    'http://localhost:3000'
+*/
+app.post('/', (req, res) =>
+  generateToken(req.body) // Using test params, we should use `req.params`
+    .then(createCharge)
+    .then(charge => res.status(201).json({ charge }))
+    .catch(error => res.status(422).json({ error }))
 )
 
 app.listen(3000)
@@ -38,5 +65,5 @@ const createCharge = token =>
   stripe.charges.create({
     amount: 2000,
     currency: 'eur',
-    source: token
+    source: token.id
   })
