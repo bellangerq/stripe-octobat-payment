32ace358778f225259b6355e5153e3e89c3dc213
Quentin Bellanger <hello@quentin-bellanger.com>
2017-12-23 17:37:18 +0100

    Dynamically change preview depending on amount and tax rate

--- javascript/preview.js
+++ javascript/preview.js
@@ -1,8 +1,17 @@
-const amount = document.querySelector('input[name=amount]')
+const amountInput = document.querySelector('input[name=amount]')
+let amount = amountInput.value
 const total = document.querySelector('.total')
+const taxRate = document.querySelector('.tax-rate').innerHTML / 100
+const exclTax = document.querySelector('.excl-tax')
+const taxAmount = document.querySelector('.tax-amount')
 
-total.innerHTML = amount.value
+total.innerHTML = parseInt(amount).toFixed(2)
+exclTax.innerHTML = (amount - amount * taxRate).toFixed(2)
+taxAmount.innerHTML = (amount * taxRate).toFixed(2)
 
-amount.addEventListener('keyup', () => {
-  total.innerHTML = amount.value
+amountInput.addEventListener('blur', () => {
+  amount = amountInput.value
+  total.innerHTML = parseInt(amount).toFixed(2)
+  exclTax.innerHTML = (amount - amount * taxRate).toFixed(2)
+  taxAmount.innerHTML = (amount * taxRate).toFixed(2)
 })
--- public/preview.js
+++ public/preview.js
@@ -1,10 +1,19 @@
 'use strict';
 
-var amount = document.querySelector('input[name=amount]');
+var amountInput = document.querySelector('input[name=amount]');
+var amount = amountInput.value;
 var total = document.querySelector('.total');
+var taxRate = document.querySelector('.tax-rate').innerHTML / 100;
+var exclTax = document.querySelector('.excl-tax');
+var taxAmount = document.querySelector('.tax-amount');
 
-total.innerHTML = amount.value;
+total.innerHTML = parseInt(amount).toFixed(2);
+exclTax.innerHTML = (amount - amount * taxRate).toFixed(2);
+taxAmount.innerHTML = (amount * taxRate).toFixed(2);
 
-amount.addEventListener('keyup', function () {
-  total.innerHTML = amount.value;
+amountInput.addEventListener('blur', function () {
+  amount = amountInput.value;
+  total.innerHTML = parseInt(amount).toFixed(2);
+  exclTax.innerHTML = (amount - amount * taxRate).toFixed(2);
+  taxAmount.innerHTML = (amount * taxRate).toFixed(2);
 });
